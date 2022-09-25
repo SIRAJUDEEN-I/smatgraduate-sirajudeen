3e97c4ebc424604ddbc1222e7b94066c397c994b
sirajudeen <110999854+SIRAJUDEEN-I@users.noreply.github.com>
2022-09-25 21:21:48 +0530

    Create ASCII_char.c
    takes char as input and give the charecter with the ASCII value

---
+++ ASCII_char.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  char chr;
+  printf("enter a charecter:");
+  scanf("%c",&chr);
+  printf("you have entered %C",chr);
+  printf("\nASCII value :%d",chr);
+  return 0;
+}
