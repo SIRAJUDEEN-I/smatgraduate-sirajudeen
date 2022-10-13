1957437cbd030f428fabac87c8c0e987b7d32a41
sirajudeen <110999854+SIRAJUDEEN-I@users.noreply.github.com>
2022-10-13 10:39:47 +0530

    Create area-square.java

---
+++ area-square.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+class FindAreaOfSquare 
+{ 
+public static void main (String[] args) 
+ { 
+ System.out.println("Enter Side of Square:"); 
+ Scanner scanner = new Scanner(System.in); 
+HAARIS INFOTECH 10 
+double side = scanner.nextDouble(); 
+double area = side*side; 
+ System.out.println("Area of Square is: "+area); 
+ } 
+} 
